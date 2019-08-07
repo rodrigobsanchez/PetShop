a6c71a68927c7bde4bf1f30f4b20e9fed22bcce0
aluno01 sala02 <aluno01sala02@targettrust.local>
2019-08-07 20:35:28 -0300

    20:35 07.08.2019

--- src/main/java/br/com/tt/petshop/repository/AnimalRepository.java
+++ src/main/java/br/com/tt/petshop/repository/AnimalRepository.java
@@ -3,6 +3,7 @@ package br.com.tt.petshop.repository;
 import br.com.tt.petshop.enums.EspecieEnum;
 import br.com.tt.petshop.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,40 +11,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//@Repository
+
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
     // foi necessario criar esse metodo pois o animal tinha clientId como atributo. No caso do clienteRepository nao é necessario.
     List<Animal> findByClienteId(Long clientId);
 
+    List<Animal> findByNome(String rex);
 
-//
-//    List<Animal> animais = new ArrayList<Animal>(Arrays.asList(
-//            new Animal("Rex", LocalDate.now(), EspecieEnum.MAMIFERO, 1L),
-//            new Animal("Nemo", LocalDate.now().minusMonths(1), EspecieEnum.PEIXE, 1L),
-//            new Animal("Velociraptor", LocalDate.now().minusYears(10000), EspecieEnum.REPTIL, 1L)
-//    ));
-//
-//
-//
-//
-//    public List<Animal> listar(Long clienteId){
-//        List<Animal> animaisDoCliente = new ArrayList<>();
-//        for(Animal animal : animais){
-//            if(animal.getClientId().equals(clienteId)){
-//                animaisDoCliente.add(animal);
-//            }
-//        }
-//        return animaisDoCliente;
-//    }
-//
-//    public void save(Animal animal) {
-//        animais.add(animal);
-//    }
-//
-////    public void delete(Animal animal) {
-////       animais.remove(animal);
-////   }
-//    public List<Animal> findAll(){
-//        return animais;
-//    }
+    List<Animal> findByDataNascimentoDataBetweenAndEspecieIs(LocalDate dataInicio, LocalDate dataFim, EspecieEnum especie);
+
+
 }
--- src/test/java/br/com/tt/petshop/repository/AnimalRepositoryIT.java
+++ src/test/java/br/com/tt/petshop/repository/AnimalRepositoryIT.java
@@ -7,18 +7,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Example;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class AnimalRepositoryIT {
 
+
+    //@AutoWired funciona como uma especie de construtor...
     @Autowired
     private AnimalRepository animalRepository;
 
@@ -42,5 +50,33 @@ public class AnimalRepositoryIT {
 
     @Test
     public void findByClienteId() {
+    }
+
+    @Test
+    @Sql("classpath:insere_rex.sql")
+    public void deveriaRetornarUmAnimalPorNome() {
+        List<Animal> list = animalRepository.findByNome("Rex");
+        assertEquals("Deveria retornar um animal", 1, list.size());
+        Animal rex = list.get(0);
+        assertEquals("O nome deveria ser Rex", "Rex", rex.getNome());
+        assertEquals("O cliente deveria ser o 133", Long.valueOf(133), rex.getCliente().getId());
+        assertEquals("Deveria ser um mamífero", EspecieEnum.MAMIFERO, rex.getEspecie());
+    }
+
+    @Test
+    @Sql("classpath:insere_brutus.sql")
+    public void deveriaRetornarAnimaisPorPeriodoEEspecie(){
+
+        LocalDate dataInicio = LocalDate.parse("2019-08-01");
+        LocalDate dataFim = LocalDate.parse("2019-08-31");
+        EspecieEnum especie = EspecieEnum.MAMIFERO;
+
+        List<Animal> list = animalRepository.findByDataNascimentoDataBetweenAndEspecieIs(dataInicio, dataFim, especie);
+
+        assertEquals("Deveria retornar 1 animal", 1, list.size());
+        Animal brutus = list.get(0);
+        assertEquals("Deveria ser o Brutus", "Brutus", brutus.getNome());
+        assertEquals("Deveria ser Mamífero", EspecieEnum.MAMIFERO, brutus.getEspecie());
+
     }
 }
